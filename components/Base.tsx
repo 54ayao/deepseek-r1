4b95043915f7b67df778945502a59d6e57f8ade0
A Yao <86733666+54ayao@users.noreply.github.com>
2025-03-26 18:40:09 +0800

    Update Base.tsx

--- components/Base.tsx
+++ components/Base.tsx
@@ -127,19 +127,7 @@ export const Base = () => {
     window.open('https://beian.miit.gov.cn/#/Integrated/recordQuery', '_blank');
   };
 
-  const onGithubBtnClick = () => {
-    if (isSiteEnv) {
-      window.open(
-        'https://github.com/TencentEdgeOne/pages-templates/blob/main/examples/deepseek-r1-edge/README_zh-CN.md',
-        '_blank'
-      );
-      return;
-    }
-    window.open(
-      'https://github.com/TencentEdgeOne/pages-templates/tree/main/examples/deepseek-r1-edge',
-      '_blank'
-    );
-  };
+
 
   const onDeployBtnClick = () => {
     if (isSiteEnv) {
@@ -150,7 +138,7 @@ export const Base = () => {
       return;
     }
     window.open(
-      'https://edgeone.ai/pages/templates/deepseek-r1-edge',
+      'https://console.cloud.tencent.com/edgeone/pages/new?from=github&template=deepseek-r1-edge',
       '_blank'
     );
   };
@@ -556,15 +544,7 @@ export const Base = () => {
               className="h-7"
             />
           </a>
-          <a
-            onClick={onGithubBtnClick}
-            target="_blank"
-            className="text-gray-600 transition-colors cursor-pointer hover:text-gray-900"
-          >
-            <svg height="24" width="24" viewBox="0 0 16 16" fill="currentColor">
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
-            </svg>
-          </a>
+         
         </header>
 
         <WelcomeMessage show={showKeywords} />
