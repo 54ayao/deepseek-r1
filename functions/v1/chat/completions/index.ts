dfe02ac0da9e881507426f551871a4c0f3229485
A Yao <86733666+54ayao@users.noreply.github.com>
2025-05-31 02:25:50 +0800

    更新 index.ts

--- functions/v1/chat/completions/index.ts
+++ functions/v1/chat/completions/index.ts
@@ -198,7 +198,7 @@ export async function onRequest({ request, params, env }: any) {
   try {
     // @ts-ignore-next-line
     const res = await AI.chatCompletions({
-      model: '@tx/deepseek-ai/deepseek-r1-distill-qwen-32b',
+      model: '@tx/deepseek-ai/deepseek-r1-0528',
       messages: messages,
       stream: true,
     });
